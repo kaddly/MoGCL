e58eba73ac0a17bf193d08b18622b84c620117b5
cxz <1838038611@qq.com>
2023-02-24 09:44:15 +0800

    update

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.io as sio
 from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import dataset, dataloader
+from torch.utils.data import Dataset, DataLoader
 
 
 def read_YelpChi(path=os.path.join(os.path.abspath('.'), 'data'), file_name='YelpChi.mat'):
@@ -36,17 +36,24 @@ def read_data(data_set):
         return read_YelpChi()
 
 
-def generator_pos_neg_nodes(nodes, relation_list):
-    all_pos_neg_nodes = []
+def generator_pos_neg_nodes(nodes, relation_list, num_pos):
+    nodes_pos, nodes_neg = [], []
     relation_homo = None
     for relation in relation_list:
         if relation_homo is None:
             relation_homo = relation
         relation_homo += relation
-    relation_homo = relation_homo.A
     for node in nodes:
-        all_pos_neg_nodes.append(relation_homo[node])
-    return all_pos_neg_nodes
+        keys = relation_homo[node].data
+        if len(keys) < num_pos:
+            pos = relation_homo[node].nonzero()[1].tolist()
+            nodes_pos.append(pos + [node for _ in (num_pos-len(keys))])
+            nodes_neg.append(np.delete(nodes, pos+node).tolist()[:len(nodes)-num_pos])
+        else:
+            pos_neg = sorted(relation_homo[node].nonzero()[1].tolist(), key=keys)
+            nodes_pos.append(pos_neg[:num_pos])
+            nodes_neg.append(np.delete(nodes, nodes_pos[node]).tolist())
+    return nodes_pos, nodes_neg
 
 
 def generator_neighbors(nodes, relation_list):
@@ -60,7 +67,7 @@ def generator_neighbors(nodes, relation_list):
     return all_neighbors
 
 
-class MultiViewDataset(dataset):
+class MultiViewDataset(Dataset):
     def __init__(self, nodes):
         super(MultiViewDataset, self).__init__()
         self.nodes = nodes
@@ -72,18 +79,31 @@ class MultiViewDataset(dataset):
         return self.nodes[item]
 
 
-class Collect_fn:
-    def __init__(self, all_neighbors, all_pos_neg_nodes, num_neigh, num_pos, num_view):
+class Collate_fn:
+    def __init__(self, all_neighbors, all_pos, all_neg, num_neigh):
         self.all_neighbors = all_neighbors
-        self.all_pos_neg_nodes = all_pos_neg_nodes
-        self.nun_neigh = num_neigh
-        self.num_pos = num_pos
-        self.num_view = num_view
+        self.num_neigh = num_neigh
+        self.all_pos = all_pos
+        self.all_neg = all_neg
 
     def __call__(self, data):
-        nodes, nodes_neigh, nodes_pos, nodes_pos_neigh = [], [], [], []
+        nodes, nodes_neigh, nodes_pos, nodes_pos_neigh, nodes_neg = [], [], [], [], []
         for node in data:
-            pass
+            nodes.append(node)
+            nodes_pos.append(self.all_pos[node])
+            nodes_neg.append(self.all_neg[node])
+            neigh = []
+            for candidates in self.all_neighbors[node]:
+                neigh.append(random.choices(candidates, k=self.num_neigh))
+            nodes_neigh.append(neigh)
+            node_pos_neigh = []
+            for pos in nodes_pos[node]:
+                pos_neigh = []
+                for candidates in self.all_neighbors[pos]:
+                    pos_neigh.append(random.choices(candidates, k=self.num_neigh))
+                node_pos_neigh.append(pos_neigh)
+            nodes_pos_neigh.append(node_pos_neigh)
+        return (torch.tensor(nodes), torch.tensor(nodes_neigh)), (torch.tensor(nodes_pos), torch.tensor(nodes_pos_neigh)), torch.tensor(nodes_neg)
 
 
 def load_data(args):
@@ -100,4 +120,8 @@ def load_data(args):
         idx_train, idx_test, y_train, y_test = train_test_split(index, labels[3305:], stratify=labels[3305:],
                                                                 test_size=args.test_size, random_state=2, shuffle=True)
     all_neighbors = generator_neighbors(list(range(len(labels))), relation_list)
-    all_pos_neg_nodes = generator_pos_neg_nodes(list(range(len(labels))), relation_list)
+    all_pos, all_neg = generator_pos_neg_nodes(list(range(len(labels))), relation_list, args.num_pos)
+    train_dataset = MultiViewDataset(idx_train)
+    collate_fn = Collate_fn(all_neighbors, all_pos, all_neg, args.num_neigh)
+    train_iter = DataLoader(dataset=train_dataset, batch_size=args.batch_size,collate_fn=collate_fn)
+    return train_iter, feat_data, (idx_test, y_test)
